7c2c6a64940dd565c38bd99a5c1b3ec4798a596b
Emlava <emiliolaravargas@hotmail.com>
2025-02-14 10:18:01 +0100

    added ex03

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,31 @@
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	char	*dest_start;
+
+	dest_start = dest;
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src && nb > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		nb--;
+	}
+	*dest = '\0';
+	return (dest_start);
+}
+
+/*#include <stdio.h>
+
+int	main()
+{
+	char dest[20] = "Hello";
+	char src[] = "! How are you?";
+	unsigned int nb = 3;
+
+	printf("%s\n", ft_strncat(dest, src, nb));
+	return (0);
+}*/
